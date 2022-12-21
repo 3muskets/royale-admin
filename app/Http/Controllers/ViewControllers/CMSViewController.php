d40f06149b5afcec2a016b4ee956fd8772a9d377
chu <chu@ciphertech.io>
2022-12-21 16:33:39 +0800

    fix

--- app/Http/Controllers/ViewControllers/CMSViewController.php
+++ app/Http/Controllers/ViewControllers/CMSViewController.php
@@ -104,7 +104,7 @@ class CMSViewController  extends Controller
 
     public function updateBanner(Request $request)
     {   
-        Helper::checkUAC('system.accounts.asubaccountdmin');
+        Helper::checkUAC('system.accounts.admin');
 
         $data = CMSController::updateBanner($request);
 
